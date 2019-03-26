dde54eeb5a067c85bc9aca6d8dc2e1edad5d278e
Oliver Marriott <olimarrio@gmail.com>
2019-03-26 09:36:35 +0100

    generated product model

---
+++ db/migrate/20190326083551_create_products.rb
@@ -0,0 +1,14 @@
+class CreateProducts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.integer :quantity
+      t.string :photo
+      t.string :price
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
